8b8a4a885d1aa3e55edbceab80a46cbdc9105a2d
qkrcodus <130819578+qkrcodus@users.noreply.github.com>
2024-05-18 20:01:00 +0900

    feat: html css 수정

--- week02/chaeyeon/calendar/index.js
+++ week02/chaeyeon/calendar/index.js
@@ -1,10 +1,12 @@
-//js엔 Date 클래스 내장
+//js엔 Date 클래스 내장되어 있다.
 //현재 날짜 
 const today= new Date();
 //현재 월 getMonth() 메서드는 1월일때 0을 반환
 let currentMonth=today.getMonth();
 //현재 년도
 let currentYear=today.getFullYear();
+//현재 날짜
+let currentDate=today.getDate();
 
 //월별로 캘린더 생성하고 표시하는 함수
 function renderCalendar(){
@@ -16,5 +18,44 @@ function renderCalendar(){
     const firstDayOfweek=firstDayOfMonth.getDay();
     //현재 년도 월 표시
     document.getElementById('current').textContent=`${currentYear}년 ${currentMonth+1}월`;
+    //캘린더 그릴 HTML
+    let daysHtml='';
+    //첫번째주 빈칸
+    for(let i=0;i<firstDayOfweek;i++){
+        daysHtml+='<li></li>';
+    }
+    //날짜 채우기
+    for(let day=1;day<=daysInMonth;day++){
+        //오늘의 날짜를 빨간 글씨로 바꾸기
+        daysHtml+=`<li>${day}</li>`;
+        
+    }
+    //캘린더 html업데이트
+    document.querySelector('.days').innerHTML=daysHtml;
+    currentDate
 }
 renderCalendar();
+//이전 달로 이동하는 함수
+function goToPreviousMonth(){
+    if(currentMonth==0){
+        currentMonth=11;
+        currentYear--;
+    }else{
+        currentMonth--;
+    }
+    renderCalendar();
+}
+//다음 달로 이동하는 함수
+function goToNextMonth(){
+    if(currentMonth==11){
+        currentMonth=0;
+        currentYear++;
+    }
+    else currentMonth++;
+    renderCalendar();
+}
+
+//이벤트 리스너 설정
+document.getElementById('prevBtn').addEventListener('click',goToPreviousMonth);
+document.getElementById('nextBtn').addEventListener('click',goToNextMonth);
+
