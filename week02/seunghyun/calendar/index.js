2e48cdb662f3cb5ea8a17f10188dc874e5d7b9ea
hyun907 <seunghyun020907@naver.com>
2024-05-19 15:00:12 +0900

    Feat: renderCalender 함수로 정리

--- week02/seunghyun/calendar/index.js
+++ week02/seunghyun/calendar/index.js
@@ -2,40 +2,44 @@
 
 const date = new Date();
 
-const viewYear = date.getFullYear();
-const viewMonth = date.getMonth();
+const renderCalender = () => {
+  const viewYear = date.getFullYear();
+  const viewMonth = date.getMonth();
 
-document.querySelector(".year-month").textContent = `${viewYear}년 ${
-  viewMonth + 1
-}월`;
+  document.querySelector(".year-month").textContent = `${viewYear}년 ${
+    viewMonth + 1
+  }월`;
 
-const prevLast = new Date(viewYear, viewMonth, 0);
-const thisLast = new Date(viewYear, viewMonth + 1, 0);
+  const prevLast = new Date(viewYear, viewMonth, 0);
+  const thisLast = new Date(viewYear, viewMonth + 1, 0);
 
-const PLDate = prevLast.getDate();
-const PLDay = prevLast.getDay();
+  const PLDate = prevLast.getDate();
+  const PLDay = prevLast.getDay();
 
-const TLDate = thisLast.getDate();
-const TLDay = thisLast.getDay();
+  const TLDate = thisLast.getDate();
+  const TLDay = thisLast.getDay();
 
-const PrevDates = [];
-const thisDates = [...Array(TLDate + 1).keys()].slice(1);
-const nextDates = [];
+  const PrevDates = [];
+  const thisDates = [...Array(TLDate + 1).keys()].slice(1);
+  const nextDates = [];
 
-if (PLDay !== 6) {
-  for (let i = 0; i < PLDay + 1; i++) {
-    PrevDates.unshift(PLDate - i);
+  if (PLDay !== 6) {
+    for (let i = 0; i < PLDay + 1; i++) {
+      PrevDates.unshift(PLDate - i);
+    }
   }
-}
 
-for (let i = 1; i < 7 - TLDay; i++) {
-  nextDates.push(i);
-}
+  for (let i = 1; i < 7 - TLDay; i++) {
+    nextDates.push(i);
+  }
 
-const dates = PrevDates.concat(thisDates, nextDates);
+  const dates = PrevDates.concat(thisDates, nextDates);
 
-dates.forEach((date, i) => {
-  dates[i] = `<div class=date>${date}</div>`;
-});
+  dates.forEach((date, i) => {
+    dates[i] = `<div class=date>${date}</div>`;
+  });
 
-document.querySelector(".dates").innerHTML = dates.join("");
+  document.querySelector(".dates").innerHTML = dates.join("");
+};
+
+renderCalender();
