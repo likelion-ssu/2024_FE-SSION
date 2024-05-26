085e4abaf16e334227451f04b1124c52661e3e6c
yminjuu <ymj0580@naver.com>
2024-05-26 15:35:17 +0900

    feat: youtube reset

--- week03/minju/youtube_cloneCoding/src/main.jsx
+++ week03/minju/youtube_cloneCoding/src/main.jsx
@@ -3,4 +3,5 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+// App 컴포넌트를 랜더링한다.
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
