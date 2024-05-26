b0d4cbf8efc685f14b521bea874a3032a1092b56
KIMGEONHWI <dirnrlarjsgn@naver.com>
2024-05-26 21:07:29 +0900

    feat: calendar 구현

---
+++ week02/week02_geonhwi/calendar/index.js
@@ -0,0 +1,55 @@
+let currentDate = new Date();
+let todayDate = new Date();
+
+buildCalender();
+
+function buildCalender() {
+  let prevLast = new Date(currentDate.getFullYear(), currentDate.getMonth(), 0);
+  let thisFirst = new Date(
+    currentDate.getFullYear(),
+    currentDate.getMonth(),
+    1
+  );
+  let thisLast = new Date(
+    currentDate.getFullYear(),
+    currentDate.getMonth() + 1,
+    0
+  );
+  document.querySelector(".year").innerHTML = currentDate.getFullYear();
+  document.querySelector(".month").innerHTML = currentDate.getMonth() + 1;
+  let dates = [];
+  if (thisFirst.getDay() != 0) {
+    for (let i = 0; i < thisFirst.getDay(); i++) {
+      dates.unshift(prevLast.getDate() - i);
+    }
+  }
+  for (let i = 1; i <= thisLast.getDate(); i++) {
+    dates.push(i);
+  }
+  for (let i = 1; i <= 13 - thisLast.getDay(); i++) {
+    dates.push(i);
+  }
+  let htmlDates = "";
+  for (let i = 0; i < 42; i++) {
+    if (
+      todayDate.getDate() == dates[i] &&
+      todayDate.getMonth() == currentDate.getMonth() &&
+      todayDate.getFullYear() == currentDate.getFullYear()
+    ) {
+      htmlDates += `<div class="date today">${dates[i]}</div>`;
+    } else {
+      htmlDates += `<div class="date">${dates[i]}</div>`;
+    }
+  }
+  document.querySelector(".dates").innerHTML = htmlDates;
+}
+
+function prevCal() {
+  currentDate.setMonth(CDate.getMonth() - 1);
+  buildCalender();
+}
+
+function nextCal() {
+  currentDate.setMonth(CDate.getMonth() + 1);
+  buildCalender();
+}
