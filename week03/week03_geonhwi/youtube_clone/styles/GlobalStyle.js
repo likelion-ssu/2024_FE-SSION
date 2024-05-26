cd1b5ea532246b2fa74f4b102ab00e4e68f95342
KIMGEONHWI <dirnrlarjsgn@naver.com>
2024-05-26 23:06:44 +0900

    chore: globalstyle 적용

---
+++ week03/week03_geonhwi/youtube_clone/styles/GlobalStyle.js
@@ -0,0 +1,56 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyle = createGlobalStyle`
+    ${reset}
+    * {
+        box-sizing: border-box;
+        font-family:
+        'Pretendard Variable',
+        Pretendard,
+        -apple-system,
+        BlinkMacSystemFont,
+        system-ui,
+        Roboto,
+        'Helvetica Neue',
+        'Segoe UI',
+        'Apple SD Gothic Neo',
+        'Noto Sans KR',
+        'Malgun Gothic',
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+        'Segoe UI Symbol',
+        sans-serif;
+    }
+    html,
+    body {
+        box-sizing: border-box; /* default */
+        margin: 0 auto;
+        font-size: 62.5%;
+    }
+    input {
+        outline: none;
+    }
+    a {
+        color: inherit;
+        text-decoration: none;
+        cursor: pointer;
+    }
+    button {
+        font: inherit;
+        background: none;
+        cursor: pointer;
+        border: none;
+        padding: 0;
+    }
+    select {
+        cursor: pointer;
+    }
+    @font-face {
+        font-family: Pretendard;
+        src: url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.6/dist/web/static/pretendard.css')
+        format('woff');
+    }
+`;
+
+export default GlobalStyle;
